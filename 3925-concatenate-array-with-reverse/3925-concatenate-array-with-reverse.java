4572d1fd946d82acbef13f05709df4474f777bd2
akshadawalve11 <akshadawalve11@gmail.com>
2026-08-25 19:41:46 +0530

    Update 3925-concatenate-array-with-reverse.java

--- 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.java
+++ 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] concatWithReverse(int[] nums) {
         int n = nums.length;
-        int[] ans = new int[2 * n];
+        int[] res = new int[2 * n];
 
         for (int i = 0; i < n; i++) {
-            ans[i] = nums[i];
-            ans[n + i] = nums[n - 1 - i];
+            res [i] = nums[i];
+            res [n + i] = nums[n - 1 - i];
         }
 
-        return ans;
+        return res;
     }
 }
